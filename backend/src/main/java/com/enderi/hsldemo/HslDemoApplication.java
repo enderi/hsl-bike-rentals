9e5a76b7358e2a5e99c80706957a0ad0e29bfe9e
Janne Vento <jannejvento@gmail.com>
2023-05-19 22:41:02 +0300

    Add generated spring boot

---
+++ backend/src/main/java/com/enderi/hsldemo/HslDemoApplication.java
@@ -0,0 +1,13 @@
+package com.enderi.hsldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HslDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HslDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enderi/hsldemo/HslDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enderi.hsldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HslDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
